90509bdcdd0ed5c8ca58578f67ec1b0376e57cd6
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-12-07 20:42:17 -0600

    Add files via upload

---
+++ BMICalculator.java
@@ -0,0 +1,24 @@
+package ScannerPractice;
+
+import java.util.Scanner;
+
+public class BMICalculator {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = input.next();
+
+        System.out.println("Please provide your date of birth: (mm/dd/yyyy)");
+        String dob = input.next();
+
+        System.out.println();
+
+
+
+
+
+
+    }
+}
